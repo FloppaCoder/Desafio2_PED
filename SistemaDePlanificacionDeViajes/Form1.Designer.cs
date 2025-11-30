05d78b03ed5ee8f449ac92bf2d5d7ac7c79c33a4
FloppaCoder <marcelo.menjivar2006@gmail.com>
2025-11-29 20:16:13 -0600

    feat: Adding UI Layout, Img Asset

--- SistemaDePlanificacionDeViajes/Form1.Designer.cs
+++ SistemaDePlanificacionDeViajes/Form1.Designer.cs
@@ -28,12 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            groupBox1 = new GroupBox();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Font = new Font("Segoe UI", 16F);
+            groupBox1.ForeColor = Color.White;
+            groupBox1.Location = new Point(888, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(364, 648);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Controles";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.map;
+            pictureBox1.Location = new Point(25, 25);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(831, 635);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(0, 25, 38);
+            ClientSize = new Size(1264, 681);
+            Controls.Add(pictureBox1);
+            Controls.Add(groupBox1);
+            ForeColor = Color.FromArgb(0, 25, 38);
+            Name = "Form1";
+            Text = "Sistema de Transporte";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private PictureBox pictureBox1;
     }
 }
